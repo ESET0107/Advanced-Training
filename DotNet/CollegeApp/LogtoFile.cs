c16d7ae5b2db0ecf8653f0ef764c06ae00ba8286
NVKoushik <nvkoushik18@gmail.com>
2025-10-27 09:52:34 +0530

    added new files

---
+++ DotNet/CollegeApp/LogtoFile.cs
@@ -0,0 +1,13 @@
+﻿using CollegeApp.Mylogger;
+
+namespace CollegeApp
+{
+    public class LogtoFile : IMylogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+            Console.WriteLine("LogtoFile");
+        }
+    }
+}
---
+++ DotNet/CollegeApp/Program.cs
@@ -0,0 +1,52 @@
+
+using CollegeApp.Data;
+using CollegeApp.Data.Repository;
+using CollegeApp.Mylogger;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+namespace CollegeApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddDbContext<CollegeDBContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionDB"));
+            });
+            builder.Services.AddControllers().AddNewtonsoftJson();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddControllers().AddNewtonsoftJson(options =>
+            {
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            });
+
+            builder.Services.AddScoped(typeof(ICollegeRepository<>), typeof(CollegeRepository<>));
+            builder.Services.AddScoped<IStudentRepository, StudentRepository>();
+            builder.Services.AddScoped<IMylogger, LogtoFile>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
